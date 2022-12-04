eafdd6a3221ab4792dda45360d4d9c4a539b5965
스프링 피바라기 <daehwan7989@naver.com>
2022-12-05 00:06:06 +0900

    Iterable 및 Iterator 인터페이스 추가

---
+++ src/main/kotlin/collection/Iterable.kt
@@ -0,0 +1,29 @@
+package collection
+
+interface Iterable<out T> {
+    operator fun iterator(): Iterator<T>
+
+    interface Iterator<out T> {
+        operator fun next(): T
+        operator fun hasNext(): Boolean
+    }
+}
+
+interface MutableIterable<out T> : Iterable<T> {
+    override fun iterator(): MutableIterator<T>
+
+    interface MutableIterator<out T> : Iterable.Iterator<T> {
+        fun remove(): Unit
+    }
+}
+
+interface ListIterator<out T> : Iterable.Iterator<T> {
+    // Query Operations
+    override fun next(): T
+    override fun hasNext(): Boolean
+
+    fun hasPrevious(): Boolean
+    fun previous(): T
+    fun nextIndex(): Int
+    fun previousIndex(): Int
+}
