d754a7a2c40afd99b4d9ddf0677cf6378447726e
스프링 피바라기 <daehwan7989@naver.com>
2022-12-05 00:05:26 +0900

    Collections listOf Test 추가

--- src/test/kotlin/collection/CollectionsTest.kt
+++ src/test/kotlin/collection/CollectionsTest.kt
@@ -1,41 +1,35 @@
 package collection
 
 import org.junit.jupiter.api.*
-import java.lang.IndexOutOfBoundsException
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-internal class ListTest {
+class CollectionsTest {
     @Test
     fun `빈 immutable 한 List 를 반환한다`() {
         assertTrue { EmptyList is List<*> }
     }
 
     @Nested
-    internal class EmptyListTest {
-        lateinit var 검증_대상: List<*>
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class EmptyListTest {
+        lateinit var emptyList: List<*>
 
         // given for each
-        @BeforeEach
+        @BeforeAll
         fun init() {
-            검증_대상 = emptyList()
+            emptyList = emptyList()
         }
 
         @Test
         fun `Empty 한 지 검증한다`() {
             // when
-            assertAll({
-                assertEquals(검증_대상.toString(), "[]")
-                assertTrue(검증_대상.isEmpty())
-                assertFalse(검증_대상.contains(""))
-                assertEquals(검증_대상.indexOf(3), -1)
-                assertEquals(검증_대상.lastIndexOf("a"), -1)
-            })
+            assertEmptyList(emptyList)
         }
 
         @Nested
-        internal class Exceptions {
+        inner class Exceptions {
             @Test
             fun `listIterator() 에서 0 이 아닌 경우 예외 발생`() {
                 assertThrows<IndexOutOfBoundsException> { emptyList().listIterator(1) }
@@ -43,5 +37,65 @@ internal class ListTest {
         }
     }
 
+    @Nested
+    inner class listOfTest {
+        lateinit var 검증_대상: List<*>
 
+        @Test
+        fun `입력한 element 가 없는 경우는 emptyList 를 응답한다`() {
+            // when
+            val listOf = listOf<String>()
+
+            // then
+            assertEmptyList(listOf)
+        }
+
+        @Test
+        fun `element 가 한개 인 경우 size 가 1인 list 를 응답한다`() {
+            // when
+            val listOf = listOf("hello world")
+
+            // then
+            assertElementsOfList(listOf, "hello world")
+        }
+
+        @Test
+        fun `element 가 여러개 인 경우 size 가 여러개인 list 를 응답한다`() {
+            // when
+            val listOf = listOf("hello", "world", "daehwan")
+
+            // then
+            assertElementsOfList(listOf, "hello", "world", "daehwan")
+        }
+
+        @Test
+        fun `element 가 여러개인 경우 순서가 보장이 된다`() {
+            // when
+            val listOf = listOf("hello", "world", "daehwan")
+
+            // then
+            assertEquals(listOf[0], "hello")
+            assertEquals(listOf[1], "world")
+            assertEquals(listOf[2], "daehwan")
+        }
+    }
+
+    private fun assertEmptyList(target: List<*>) {
+        assertAll({
+            assertEquals(target.toString(), "[]")
+            assertTrue(target.isEmpty())
+            assertFalse(target.contains(""))
+            assertEquals(target.indexOf(3), -1)
+            assertEquals(target.lastIndexOf("a"), -1)
+        })
+    }
+
+    private fun <T> assertElementsOfList(target: List<*>, vararg t: T) {
+        assertAll({
+            t.forEach { assertTrue(target.contains(it)) }
+            assertTrue(target.isNotEmpty())
+            assertEquals(target.lastIndexOf("a"), -1)
+            assertEquals(target.size, t.size)
+        })
+    }
 }
