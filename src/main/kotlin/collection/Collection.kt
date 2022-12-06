9da3ed71f4cabb710c01d97593cd7c11068aad3f
스프링 피바라기 <daehwan7989@naver.com>
2022-12-06 23:36:03 +0900

    add Map and MutableMap in Collection.kt

--- src/main/kotlin/collection/Collection.kt
+++ src/main/kotlin/collection/Collection.kt
@@ -94,3 +94,72 @@ interface MutableSet<E> : Set<E>, MutableCollection<E> {
 
     override fun clear()
 }
+
+interface Map<K, out V> {
+    val size: Int
+
+    fun isEmpty(): Boolean
+    fun containsKey(key: K): Boolean
+    fun containsValue(value: @UnsafeVariance V): Boolean
+    operator fun get(key: K): V? // nullable
+
+    @SinceKotlin("1.1")
+    fun getOrDefault(key: K, defaultValue: @UnsafeVariance V): V {
+        // 내부가 반드시 로직으로 구현되어야한다.
+        throw NotImplementedError()
+    }
+
+    // Views
+    /**
+     * Returns a read-only Set of all keys in this map.
+     * key can not be duplicated
+     */
+    val keys: Set<K>
+
+    /**
+     * Returns a read-only Collection of all values in this map.
+     * values can be duplicated
+     */
+    val values: Collection<V>
+    val entries: Set<Entry<K, V>>
+
+    interface Entry<out K, out V> {
+        val key: K
+        val value: V
+    }
+}
+
+interface MutableMap<K, V> : Map<K, V> {
+    // Modification Operations
+    fun put(key: K, value: V): V? // nullable
+    fun remove(key: K): V? // nullable
+
+    @SinceKotlin("1.1")
+    fun remove(key: K, value: V): Boolean {
+        // 아무 행위를 수행하지 않더라도 기본적으로 true 를 응답한다. -> idempotent
+        return true
+    }
+
+    // Bulk Operations
+    /**
+     * from 으로부터 Entry 들을 해당 map 에 채워넣는다.
+     */
+    fun putAll(from: Map<K, V>): Unit
+
+    /**
+     * 모든 element 를 지운다.
+     */
+    fun clear(): Unit
+
+    override val keys: MutableSet<K>
+    override val values: MutableCollection<V>
+    override val entries: Set<MutableEntry<K, V>>
+
+    interface MutableEntry<K, V> : Map.Entry<K, V> {
+        /**
+         * 새로운 값으로 해당 Entry 의 value 를 변경한다.
+         * 응답으로는 기존 값을 반환한다.
+         */
+        fun setValue(newValue: V): V
+    }
+}
