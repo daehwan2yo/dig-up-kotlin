ec038263cbdd0f53b7c94f3e65c403557f618b78
스프링 피바라기 <daehwan7989@naver.com>
2022-12-06 22:36:04 +0900

    add List and MutableList in Collection.kt

--- src/main/kotlin/collection/Collection.kt
+++ src/main/kotlin/collection/Collection.kt
@@ -11,7 +11,7 @@ interface Collection<out E> : Iterable<E> {
     override fun iterator(): Iterable.Iterator<E>
 }
 
-interface MutableCollection<E>: Collection<E>, MutableIterable<E> {
+interface MutableCollection<E> : Collection<E>, MutableIterable<E> {
     override fun iterator(): MutableIterable.MutableIterator<E>
 
     fun add(element: E): Boolean
@@ -20,4 +20,51 @@ interface MutableCollection<E>: Collection<E>, MutableIterable<E> {
     fun removeAll(elements: Collection<E>): Boolean
     fun retainAll(elements: Collection<E>): Boolean
     fun clear(): Unit
+}
+
+interface List<out E> : Collection<E> {
+    override val size: Int
+    override fun isEmpty(): Boolean
+    override fun contains(elements: @UnsafeVariance E): Boolean
+    override fun iterator(): Iterable.Iterator<E>
+
+    // Bulk Operation
+    override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
+
+    // Positional Access Operations
+    operator fun get(index: Int): E
+
+    // Search Operations
+    fun indexOf(element: @UnsafeVariance E): Int
+    fun lastIndexOf(element: @UnsafeVariance E): Int
+
+    // List Iterators
+    fun listIterator(): ListIterator<E>
+    fun listIterator(index: Int): ListIterator<E>
+
+    // View
+    fun subList(fromIndex: Int, toIndex: Int): List<E>
+}
+
+interface MutableList<E> : List<E>, MutableCollection<E> {
+    override fun add(element: E): Boolean
+    override fun remove(element: E): Boolean
+
+    // Bulk Operations
+    override fun addAll(elements: Collection<E>): Boolean
+    override fun removeAll(elements: Collection<E>): Boolean
+    override fun retainAll(elements: Collection<E>): Boolean
+    override fun clear(): Unit
+
+    // Positional Access Operations
+    operator fun set(index: Int, element: E): E
+    fun add(index: Int, element: E): Unit
+    fun removeAt(index: Int): E
+
+    // List Iterator
+    override fun listIterator(): MutableListIterator<E>
+    override fun listIterator(index: Int): MutableListIterator<E>
+
+    // View
+    override fun subList(fromIndex: Int, toIndex: Int): MutableList<E>
 }
